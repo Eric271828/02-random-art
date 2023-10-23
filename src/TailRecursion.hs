4cf62d0da3d8d7a2f12ca0a7f8151dcc2ea53e14
Eric271828 <147651470+Eric271828@users.noreply.github.com>
2023-10-23 01:21:02 +0000

    changing from y to x

--- src/TailRecursion.hs
+++ src/TailRecursion.hs
@@ -181,7 +181,7 @@ collatz n
 fixpointW :: (Int -> Int) -> Int -> Int
 fixpointW f x = wwhile wwf x
  where
-   wwf y       = if (f y == y) then (False, y) else  (True, f y)
+   wwf x       = if (f x == x) then (False, x) else  (True, f x)
 
 -- >>> fixpointW collatz 1
 -- 1
